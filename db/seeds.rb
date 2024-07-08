84c282997b63a483ca7e6ab9b73b92663738d884
ZarathustraSama <mr.gabriel@live.it>
2024-07-08 11:32:24 +0200

    Created seed with codes/flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%w[AAL AES AAR YXX ABZ ABR ABJ ABI AUH ABV ACA SFO NYC].each do |code|
+  Airport.find_or_create_by!(code:)
+end
+
+Flight.create!([{
+                 departure_id: 1,
+                 arrival_id: 2,
+                 start_datetime: '2024-09-11 09:00:00',
+                 flight_duration: 130
+               },
+                {
+                  departure_id: 4,
+                  arrival_id: 1,
+                  start_datetime: '2024-09-11 13:30:00',
+                  flight_duration: 220
+                },
+                {
+                  departure_id: 2,
+                  arrival_id: 1,
+                  start_datetime: '2025-11-12 12:00:00',
+                  flight_duration: 150
+                },
+                {
+                  departure_id: 3,
+                  arrival_id: 4,
+                  start_datetime: '2024-08-08 20:00:00',
+                  flight_duration: 90
+                }])
