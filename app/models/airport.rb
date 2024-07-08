9ef3d2b8303b22622d4be313d10c2485ac1d792f
ZarathustraSama <mr.gabriel@live.it>
2024-07-08 11:35:11 +0200

    Added uniqueness to code column, added associations between airports and flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ApplicationRecord
+  validates :code, uniqueness: true
+
+  has_many :departing_flights, foreign_key: 'departure_id', class_name: 'Flight'
+  has_many :arriving_flights, foreign_key: 'arrival_id', class_name: 'Flight'
 end
