beea0db6f0e3cd1791fa3e0c322cc7ad77535da4
ZarathustraSama <mr.gabriel@live.it>
2024-07-08 11:53:19 +0200

    Optimezed logic for handling instance variables, now airports is a simple list and the datetime is without duplicates

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -1,14 +1,8 @@
 class FlightController < ApplicationController
   def index
     @flights = Flight.where(flight_params.except[:passengers])
-    @departing_options = []
-    @arrival_options = []
-    @datetime_options = []
-    Flight.all.map do |f|
-      @departing_options << [f.departure_airport.code, f.departure_airport.id]
-      @arrival_options << [f.arrival_airport.code, f.arrival_airport.id]
-      @datetime_options << [f.start_datetime.strftime('%d/%m/%y'), f.start_datetime.to_date]
-    end
+    @airport_codes = Airport.all.map { |a| [a.code, a.id] }
+    @datetime_options = Flight.all.map { |f| [f.start_datetime.strftime('%d/%m/%y'), f.start_datetime.to_date] }.uniq
   end
 
   private
