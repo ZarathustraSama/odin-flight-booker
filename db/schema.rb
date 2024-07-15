a5dab54834832d12927fbb98ca22ad32a220efd1
ZarathustraSama <mr.gabriel@live.it>
2024-07-15 11:07:36 +0200

    Migrated foreign key to passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_094342) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_090629) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_12_094342) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
