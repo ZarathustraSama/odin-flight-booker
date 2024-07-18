9ebb9966b2779e7cae51c4b7e696fe52ef7a790b
ZarathustraSama <mr.gabriel@live.it>
2024-07-18 09:15:17 +0200

    Removed passenger_id from bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_090629) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_071416) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_090629) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "flight_id"
-    t.integer "passenger_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
